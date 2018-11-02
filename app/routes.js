e866954c07b399a95be3ddec62b66fef113f291c
Ben Hazell <ben.hazell@digital.cabinet-office.gov.uk>
2018-11-02 15:13:15 +0000

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+
+router.post("/juggling-balls-answer", function(req,res){
+
+  var jugglingBalls = req.session.data['juggling-balls']
+
+  if (jugglingBalls == "Lots of balls"){res.redirect("/juggling-trick")}
+  else {res.redirect("/ineligable")}
+})
+
+module.exports = router
